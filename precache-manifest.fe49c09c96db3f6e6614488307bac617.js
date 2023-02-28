262219b18343e4ecb8d7cdefa936c698af2500d7
Coolin <mcmurtraycolin@gmail.com>
2023-02-27 22:14:02 -0500

    Updates

---
+++ precache-manifest.fe49c09c96db3f6e6614488307bac617.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2eb276b17a3d25f5bbe",
+    "url": "/reactPortfolio/static/js/main.d2eb276b.chunk.js"
+  },
+  {
+    "revision": "85966736835b6aed91e7",
+    "url": "/reactPortfolio/static/css/1.aa14c7b4.chunk.css"
+  },
+  {
+    "revision": "85966736835b6aed91e7",
+    "url": "/reactPortfolio/static/js/1.85966736.chunk.js"
+  },
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "a2c1cd682eae4853d0809e0d030be46a",
+    "url": "/reactPortfolio/static/media/PXL_20220627_185955559.a2c1cd68.jpg"
+  },
+  {
+    "revision": "629b41db50edf37a78b49d888fcd4eb5",
+    "url": "/reactPortfolio/static/media/Screenshot 2022-11-01 184004.629b41db.png"
+  },
+  {
+    "revision": "4943df246db150fe835bdd233e0943e8",
+    "url": "/reactPortfolio/static/media/Screenshot 2023-02-27 010038.4943df24.png"
+  },
+  {
+    "revision": "b521f5627b718b3d55b61eb4778c1920",
+    "url": "/reactPortfolio/static/media/Screenshot 2023-02-16 220621.b521f562.png"
+  },
+  {
+    "revision": "10739097e9e44b044cd0aee2b88b4bd1",
+    "url": "/reactPortfolio/static/media/216521080-b8715e4d-ff57-4dc3-bf47-58db92542cfd.10739097.png"
+  },
+  {
+    "revision": "6073c711ea3f8ab514590adace314f4b",
+    "url": "/reactPortfolio/static/media/Screenshot 2023-02-20 134924.6073c711.png"
+  },
+  {
+    "revision": "957f485ff40eeb83acc86131dea1011a",
+    "url": "/reactPortfolio/static/media/198859584-5e880983-e237-40c7-a936-3ef0d05e6b39.957f485f.png"
+  },
+  {
+    "revision": "d33df50c8bd7339c58db4eff887320c2",
+    "url": "/reactPortfolio/static/media/Post-BootCamp.d33df50c.docx"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/reactPortfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/reactPortfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/reactPortfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/reactPortfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/reactPortfolio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "78c4cf7b1b4d2eea74eee30600d0e41d",
+    "url": "/reactPortfolio/index.html"
+  }
+];
